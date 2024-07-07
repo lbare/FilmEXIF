2f4ab10c2d0285db1bd07a6613fcb206ec702d43
Levi <47727379+lbare@users.noreply.github.com>
2024-07-07 08:09:40 -0700

    Trying to get things working

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename={process.env.NODE_ENV === "production" ? "/FilmEXIF" : ""}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { config as dotenvConfig } from "dotenv";
 
+// Load environment variables
 dotenvConfig();
 
 export default defineConfig({
-  base: process.env.VITE_BASE_URL,
+  base: process.env.NODE_ENV === "production" ? process.env.VITE_BASE_URL : "/",
   plugins: [react()],
 });
