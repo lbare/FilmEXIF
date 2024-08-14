0bfa471a127161933ba769409e2e808fd9f0816d
Levi <47727379+lbare@users.noreply.github.com>
2024-08-14 14:16:05 -0700

    Setup mantine UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { MantineProvider } from "@mantine/core";
+import TabNavigator from "./navigation/TabNavigator";
+import "@mantine/core/styles.css";
 
 const App: React.FC = () => {
   useEffect(() => console.log("App mounted"), []);
 
   return (
-    <Router basename="/FilmEXIF">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <MantineProvider>
+      <Router basename="/FilmEXIF">
+        <Routes>
+          <Route path="/" element={<Navigate to="/tabs/home" replace />} />
+          <Route path="/tabs/:tabValue" element={<TabNavigator />} />
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
 };
 
