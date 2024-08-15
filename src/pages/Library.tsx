f3ae4c93a2667a01969c523f6597c21ed5936421
Levi <47727379+lbare@users.noreply.github.com>
2024-08-15 10:16:37 -0700

    Test location

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -8,18 +8,51 @@ const Library = () => {
   const { rolls, addPhotoToRoll } = useRolls();
 
   const addPhoto = (roll: FilmRoll) => {
-    console.log(roll);
+    const handleLocationSuccess = (position: GeolocationPosition) => {
+      const { latitude, longitude } = position.coords;
 
-    const newPhoto = {
-      location: {
-        latitude: 0,
-        longitude: 0,
-      },
-      date: new Date().toISOString(),
+      const newPhoto = {
+        location: {
+          latitude,
+          longitude,
+        },
+        date: new Date().toISOString(),
+      };
+
+      addPhotoToRoll(roll.id, newPhoto);
+      console.log("New photo added with location:", newPhoto);
     };
 
-    addPhotoToRoll(roll.id, newPhoto);
-    console.log(rolls);
+    const handleLocationError = (error: GeolocationPositionError) => {
+      console.error("Error getting location:", error);
+      const newPhoto = {
+        location: {
+          latitude: 0,
+          longitude: -10,
+        },
+        date: new Date().toISOString(),
+      };
+
+      addPhotoToRoll(roll.id, newPhoto);
+    };
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        handleLocationSuccess,
+        handleLocationError
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+      const newPhoto = {
+        location: {
+          latitude: -10,
+          longitude: 0,
+        },
+        date: new Date().toISOString(),
+      };
+
+      addPhotoToRoll(roll.id, newPhoto);
+    }
   };
 
   return (
@@ -28,52 +61,70 @@ const Library = () => {
       <div className="grid gap-4">
         {rolls.length > 0 ? (
           rolls.map((roll, index) => (
-            <Box
-              key={index}
-              className="flex items-center justify-between flex-row p-4 border border-neutral-500 rounded-lg"
-            >
-              <Box className="flex flex-row items-center">
-                <RingProgress
-                  size={50}
-                  thickness={3}
-                  roundCaps
-                  label={
-                    <Center>
-                      <Text className="text-lg font-semibold">
-                        {roll.photos.length + 1}
+            <>
+              <Box
+                key={index}
+                className="flex items-center justify-between flex-row p-4 border border-neutral-500 rounded-lg"
+              >
+                <Box className="flex flex-row items-center">
+                  <RingProgress
+                    size={50}
+                    thickness={3}
+                    roundCaps
+                    label={
+                      <Center>
+                        <Text className="text-lg font-semibold">
+                          {roll.photos.length + 1}
+                        </Text>
+                      </Center>
+                    }
+                    sections={[
+                      { value: roll.photos.length + 1 / 0.36, color: "blue" },
+                    ]}
+                  />
+                  <Box className="flex flex-col items-start px-4 justify-center">
+                    <Text className="text-lg font-semibold leading-tight -mt-1">
+                      {roll.name}
+                    </Text>
+                    <Box className="flex flex-row w-full items-center justify-start">
+                      <FilmStrip size={16} color="#737373" weight="regular" />
+                      <Text className="pl-2 text-xs text-neutral-500 leading-tight">
+                        {`${roll.filmBrand} ${roll.filmName} ${roll.iso}`}
                       </Text>
-                    </Center>
-                  }
-                  sections={[
-                    { value: roll.photos.length + 1 / 0.36, color: "blue" },
-                  ]}
-                />
-                <Box className="flex flex-col items-start px-4 justify-center">
-                  <Text className="text-lg font-semibold leading-tight -mt-1">
-                    {roll.name}
-                  </Text>
-                  <Box className="flex flex-row w-full items-center justify-start">
-                    <FilmStrip size={16} color="#737373" weight="regular" />
-                    <Text className="pl-2 text-xs text-neutral-500 leading-tight">
-                      {`${roll.filmBrand} ${roll.filmName} ${roll.iso}`}
-                    </Text>
-                  </Box>
-                  <Box className="flex flex-row w-full items-center justify-start">
-                    <Camera size={16} color="#737373" weight="regular" />
-                    <Text className="pl-2 text-xs text-neutral-500 leading-tight">
-                      {roll.camera}
-                    </Text>
+                    </Box>
+                    <Box className="flex flex-row w-full items-center justify-start">
+                      <Camera size={16} color="#737373" weight="regular" />
+                      <Text className="pl-2 text-xs text-neutral-500 leading-tight">
+                        {roll.camera}
+                      </Text>
+                    </Box>
                   </Box>
                 </Box>
+                <Button
+                  className="w-14 h-14 rounded-xl"
+                  variant="light"
+                  onClick={() => addPhoto(roll)}
+                >
+                  <Plus size={30} />
+                </Button>
               </Box>
-              <Button
-                className="w-14 h-14 rounded-xl"
-                variant="light"
-                onClick={() => addPhoto(roll)}
+              <Box
+                key={index}
+                className="flex items-center justify-between flex-col p-4 border border-neutral-500 rounded-lg"
               >
-                <Plus size={30} />
-              </Button>
-            </Box>
+                {roll.photos.length > 0 &&
+                  roll.photos.map((photo, index) => (
+                    <Box
+                      key={index}
+                      className="flex flex-row items-center justify-start"
+                    >
+                      <Text className="text-xs text-neutral-500">
+                        {photo.location.latitude}, {photo.location.longitude}
+                      </Text>
+                    </Box>
+                  ))}
+              </Box>
+            </>
           ))
         ) : (
           <Text className="text-center text-neutral-500">
