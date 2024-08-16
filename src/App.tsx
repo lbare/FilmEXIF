3685222d66118c5c654bb2e0441cd955df2fc7d1
Levi <47727379+lbare@users.noreply.github.com>
2024-08-16 09:51:00 -0700

    Add wildcard route

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Navigate to="/tabs/library" replace />} />
             <Route path="/tabs/:tabValue" element={<TabNavigator />} />
+            <Route path="*" element={<Navigate to="/tabs/library" replace />} />
           </Routes>
         </Router>
       </RollsProvider>
